c494349c89093ce18ad8648edbd2229bb597261e
hansungjoo <21700768@handong.edu>
2019-05-27 06:27:17 -0700

    wak

--- ChatServer.java
+++ ChatServer.java
@@ -1,3 +1,5 @@
+//git hub address https://github.com/jpnop/SimpleChat
+
 import java.net.*;
 import java.io.*;
 import java.util.*;
@@ -17,6 +19,7 @@ public class ChatServer {
 		}catch(Exception e){
 			System.out.println(e);
 		}
+
 	} // main
 }
 
@@ -32,6 +35,8 @@ class ChatThread extends Thread{
 		try{
 			PrintWriter pw = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));
 			br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			pw.println("#Do not ask private information");
+			pw.flush();
 			id = br.readLine();
 			broadcast(id + " entered.");
 			System.out.println("[Server] User (" + id + ") entered.");
@@ -51,9 +56,16 @@ class ChatThread extends Thread{
 					break;
 				if(line.indexOf("/to ") == 0){
 					sendmsg(line);
-				}else
+				}
+				else if(line.indexOf("/userlist")==0){
+					send_userlist(id);
+				}
+				else{
+					if(ban(line,id)!=1)
 					broadcast(id + " : " + line);
+				}
 			}
+
 		}catch(Exception ex){
 			System.out.println(ex);
 		}finally{
@@ -87,9 +99,45 @@ class ChatThread extends Thread{
 			Iterator iter = collection.iterator();
 			while(iter.hasNext()){
 				PrintWriter pw = (PrintWriter)iter.next();
+				if(pw==hm.get(id)) continue; //if pw of user id is same with pw, skip and continue.
 				pw.println(msg);
 				pw.flush();
 			}
 		}
 	} // broadcast
+
+	public int ban(String msg,String id){		
+		String[] no={"banknumber","phonenumber","passportnumber","residentnumber","bankpassword"};
+		int p=0;
+                for(int i=0; i<5;i++){
+                   if(msg.indexOf(no[i])!=-1) p=1;
+                   }
+		if(p==1){
+			Object obj=hm.get(id);
+			if(obj!=null){
+				PrintWriter pw=(PrintWriter)obj;
+				pw.println("You shut up! your sentence contains ban word!");
+				pw.flush();
+			}
+		}
+		return p;
+	} // if user typed such ban words using indexOf function, warnning sentence prints.
+          
+	public void send_userlist(String id){
+		int count=0;
+		PrintWriter pw=(PrintWriter)hm.get(id);
+		Set set=hm.keySet();
+		Iterator iteration=set.iterator();
+		while(iteration.hasNext()){
+			String temp=(String)iteration.next();
+			pw.println(temp);
+			pw.flush();
+			count++;
+		}
+		pw.println("number of user : "+ count);
+		pw.flush();
+	}//KeySet function combines all of user id, Iterator reads one by one and then, prints id and counts.
+
+
+
 }
